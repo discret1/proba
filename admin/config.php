61f42d2f7d5043db65d0a69b66df92210763dcdf
SSV <samokhvalovsv@mail.ru>
2014-04-09 11:54:21 +0600

    Исходный комит

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/magaz/admin/');
+define('HTTP_CATALOG', 'http://localhost/magaz/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/magaz/admin/');
+define('HTTPS_CATALOG', 'http://localhost/magaz/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\OpenServer\domains\localhost\magaz/admin/');
+define('DIR_SYSTEM', 'C:\OpenServer\domains\localhost\magaz/system/');
+define('DIR_DATABASE', 'C:\OpenServer\domains\localhost\magaz/system/database/');
+define('DIR_LANGUAGE', 'C:\OpenServer\domains\localhost\magaz/admin/language/');
+define('DIR_TEMPLATE', 'C:\OpenServer\domains\localhost\magaz/admin/view/template/');
+define('DIR_CONFIG', 'C:\OpenServer\domains\localhost\magaz/system/config/');
+define('DIR_IMAGE', 'C:\OpenServer\domains\localhost\magaz/image/');
+define('DIR_CACHE', 'C:\OpenServer\domains\localhost\magaz/system/cache/');
+define('DIR_DOWNLOAD', 'C:\OpenServer\domains\localhost\magaz/download/');
+define('DIR_LOGS', 'C:\OpenServer\domains\localhost\magaz/system/logs/');
+define('DIR_CATALOG', 'C:\OpenServer\domains\localhost\magaz/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'magaz');
+define('DB_PREFIX', 'oc_');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/magaz/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/magaz/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\OpenServer\domains\localhost\magaz/catalog/');
+define('DIR_SYSTEM', 'C:\OpenServer\domains\localhost\magaz/system/');
+define('DIR_DATABASE', 'C:\OpenServer\domains\localhost\magaz/system/database/');
+define('DIR_LANGUAGE', 'C:\OpenServer\domains\localhost\magaz/catalog/language/');
+define('DIR_TEMPLATE', 'C:\OpenServer\domains\localhost\magaz/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\OpenServer\domains\localhost\magaz/system/config/');
+define('DIR_IMAGE', 'C:\OpenServer\domains\localhost\magaz/image/');
+define('DIR_CACHE', 'C:\OpenServer\domains\localhost\magaz/system/cache/');
+define('DIR_DOWNLOAD', 'C:\OpenServer\domains\localhost\magaz/download/');
+define('DIR_LOGS', 'C:\OpenServer\domains\localhost\magaz/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'magaz');
+define('DB_PREFIX', 'oc_');
+?>
